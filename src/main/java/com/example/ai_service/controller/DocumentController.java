f75caef54b5771d25bb6b07003349b4198cbb971
정지수 <tjdals7600@gmail.com>
2026-08-12 12:16:21 +0900

    TXT 추출 기능 추가

--- src/main/java/com/example/ai_service/controller/DocumentController.java
+++ src/main/java/com/example/ai_service/controller/DocumentController.java
@@ -1,5 +1,9 @@
 package com.example.ai_service.controller;
 
+import java.io.IOException;
+
+import com.example.ai_service.service.DocumentService;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,8 +14,14 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api/documents")
 public class DocumentController {
 
+    private final DocumentService documentService;
+
+    public DocumentController(DocumentService documentService) {
+        this.documentService = documentService;
+    }
+
     @PostMapping("/summarize")
-    public String summarize(@RequestParam("file") MultipartFile file) {
-        return file.getOriginalFilename();
+    public String summarize(@RequestParam("file") MultipartFile file) throws IOException {
+        return documentService.extractText(file);
     }
 }
